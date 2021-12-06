47b28650deeddf1ce79a3c3d5a732826b2c371f9
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2021-12-06 10:46:48 -0800

    sfae primt

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    counted = 0
+    count = 0
+
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
+            print("{}".format(my_list[i]), end='')
         except:
             break
         else:
-            counted += 1
+            count += 1
+
     print()
-    return(counted)
+    return (count)
