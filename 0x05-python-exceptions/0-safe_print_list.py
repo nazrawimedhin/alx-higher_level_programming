c9edc2d40f8c1ab7533f28f5355be47f80b31472
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2021-12-06 11:06:27 -0800

    safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,10 +4,9 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
+            count += 1
         except IndexError:
             break
-        else:
-            counted += 1
-        print('')
+    print('')
     return(counted)
 
