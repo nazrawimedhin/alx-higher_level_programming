3eda083663b816207689905cee24d2dc3de648ba
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2021-12-10 00:02:12 -0800

    doc style fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-empty class that defines a square.
+Empty class that defines a square.
 """
+
 class Square:
     """
     Class Square that defines a square.
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""
+Non empty class.
+"""
 class Square:
-    """ A class that defines a square by its size
+    """A class that defines a square by its size.
     """
     def __init__(self, size=0):
-        """ Method to initialize the square object
+        """Method to initialize the square object.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
