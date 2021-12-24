b1caa8f9a95e4ae41f8547f0c272b49cc561ca3e
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2021-12-24 01:01:15 -0800

    corrrected pycode style

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def lookup(obj):
-    """
-    Looks for attributes inside of the passed object.
+    """Looks for attributes inside of the passed object
+    
+    Args:
+        obj: instance of the class
+        
+    Returns:
+        list of attribute 
     """
     return list(obj.__dict__)
