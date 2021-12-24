6ac83da2aeb684cf65d04279e9b31e5b3f8ae516
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2021-12-24 05:45:54 -0800

    task 2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A class inherite form a list class
+"""
+class MyList(list):
+    """[A class inherited from list]
+
+    Args:
+        list ([class]): [super class of MyList class]
+    """
+    def print_sorted(self):
+        sorted_list = self.copy()
+        sorted_list.sort()
+        print(sorted_list)
