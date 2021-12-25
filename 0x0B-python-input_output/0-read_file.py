621fbbce2ecbb6d83f2ccff0b9ac1808f54be062
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2021-12-25 13:19:16 -0800

    doc style fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,12 @@
+"""
+A function which reads a file
+"""
 def read_file(filename=""):
+    """A function which reads a file
+
+    Args:
+        filename (str): [the file that will be readed by th function].
+    """
     with open("filename", encoding="utf-8", mode="r") as my_file:
         my_file = filename.read()
         print(my_file)
