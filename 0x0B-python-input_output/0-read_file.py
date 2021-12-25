14a1332b78091ccf953b4a4ce04c33decd0539dd
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2021-12-25 13:16:07 -0800

    code fix, to print the file readed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,3 +1,4 @@
 def read_file(filename=""):
-    with open("filename", encoding="utf-8", mode="r"):
-        filename.read()
+    with open("filename", encoding="utf-8", mode="r") as my_file:
+        my_file = filename.read()
+        print(my_file)
