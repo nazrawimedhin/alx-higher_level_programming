efdcd399784d7643ce620c8644756b0a136c9149
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2021-12-27 12:27:01 -0800

    file 5 & 7

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 import json
 """
-A function that writes
+A function that writes an Object to a text file, using a JSON representation
 """
 
 
 def save_to_json_file(my_obj, filename):
-    """ A function that writes
+    """ A function that writes an Object to a text file, using a JSON representation
 
     Args:
-        my_obj ([dict]): [python object to create the jcon file]
+        my_obj ([dict]): [python object to create the json file]
         filename ([json]): [json file which will be created]
     """
     with open(filename, "w", encoding="utf-8") as my_file:
