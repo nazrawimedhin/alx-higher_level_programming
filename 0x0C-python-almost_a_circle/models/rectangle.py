04a2874683741a32a90025194ebe3dcad43a68c5
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2022-01-11 12:35:40 -0800

    task 4

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,3 +73,11 @@ class Rectangle(Base):
         if value <= 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+        
+    def area(self):
+        """A function that calculates area of a rectangle
+
+        Returns:
+            [int]: [area of the rectangle]
+        """
+        return self.width * self.height
