16a7f71e034f6185e370977bc2a3f88a456b61b9
Abiti <nazrawimedhin1@gmail.com>
2022-03-11 16:32:49 +0300

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+function factorial(num) {
+    if(isNaN(num)){
+        return 1;
+    }
+    else if(num === 1){
+        return 1;
+    }
+    else{
+        return num * factorial(num - 1);
+    }
+}
+
+const result = factorial(parseInt(process.argv[2]));
+console.log(result);
