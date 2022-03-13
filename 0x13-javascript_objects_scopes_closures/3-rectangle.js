4f41d8e74552056312f4e1f0dd2884adf3a8bfe7
Abiti <nazrawimedhin1@gmail.com>
2022-03-13 22:48:49 +0300

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+class Rectangle{
+    constructor(w, h) {
+        if ((w > 0) && (h > 0)){
+            this.width = w;
+            this.height = h;
+        }
+        function print(){
+            let rect = '';
+            for (let i = 0; i < h; i++) {
+                for (let j = 0; j < w; j++) {
+                    rect += 'X';
+                }
+                rect += '\n';
+            }
+            console.log(rect);
+        }
+    }
+}
+
+module.exports = Rectangle;
