ab68937c9bad096570c6e0a335be1fd7f45af072
Abiti <nazrawimedhin1@gmail.com>
2022-03-14 12:14:23 +0300

    task 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement){
+    let n_occurs = 0;
+    for (let i = 0; i < list.length; i++) {
+        if(searchElement === list[i]){
+            n_occurs++;
+        }
+    }
+    return n_occurs;
+}
