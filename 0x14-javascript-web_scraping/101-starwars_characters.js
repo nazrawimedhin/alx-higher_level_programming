19aab6d8ec17e265f5a0432e1581bd120fd4a207
Nazrawi Medhin <nazrawimedhin1@gmail.com>
2022-06-15 09:31:06 +0300

    recorrection

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,24 +1,30 @@
 #!/usr/bin/node
-// Prints all characters of a Star Wars movie in order of the list of characters
-// in /films/, with the episode's number passed as argument
-
 const request = require('request');
-const episode = process.argv[2];
+const url = 'http://swapi.co/api/films/';
+let id = parseInt(process.argv[2], 10);
+let characters = [];
 
-request('https://swapi-api.hbtn.io/api/films/' + episode, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const json = JSON.parse(body);
-    json.characters.forEach(character => {
-      request(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const json = JSON.parse(body);
-          console.log(json.name);
+request(url, function (err, response, body) {
+  if (err == null) {
+    const resp = JSON.parse(body);
+    const results = resp.results;
+    if (id < 4) {
+      id += 3;
+    } else {
+      id -= 3;
+    }
+    for (let i = 0; i < results.length; i++) {
+      if (results[i].episode_id === id) {
+        characters = results[i].characters;
+        break;
+      }
+    }
+    for (let j = 0; j < characters.length; j++) {
+      request(characters[j], function (err, response, body) {
+        if (err == null) {
+          console.log(JSON.parse(body).name);
         }
       });
-    });
+    }
   }
 });
